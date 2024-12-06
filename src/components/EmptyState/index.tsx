f8ae9011acf1e9d277daa6b3ea1ce6f4dcadccc8
araldicamila <araldi.cami@gmail.com>
2024-12-06 13:06:30 -0300

    chore: update theme empty state image

--- src/components/EmptyState/index.tsx
+++ src/components/EmptyState/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import {useTheme} from 'styled-components';
 import EmptyStateImage from '../../assets/images/empty-state.svg';
+import EmptyStateDarkImage from '../../assets/images/empty-state-dark.svg';
 import {EmptyStateContainer} from './styles';
 
 const EmptyState = () => {
+    const theme = useTheme();
+
     return (
         <EmptyStateContainer data-testid="empty-state-container">
-            <img src={EmptyStateImage} alt="No data found" />
+            <img
+                src={theme.background ? EmptyStateDarkImage : EmptyStateImage}
+                alt="No data found"
+            />
 
             <p>No data found!</p>
         </EmptyStateContainer>
